a25f051eba79e2365dd04042ea1d6d648debd795
tayp <dharkflower@gmail.com>
2026-04-25 20:39:25 -0600

    small UI fixes

--- src/Symfonicat/Controller/Admin/EnvController.php
+++ src/Symfonicat/Controller/Admin/EnvController.php
@@ -56,31 +56,6 @@ final class EnvController extends AbstractController
         ]);
     }
 
-    #[Route('/admin/e/{id}/edit', name: 'app_env_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, string $id, EnvRepository $envRepository, EntityManagerInterface $entityManager): Response
-    {
-        $env = $envRepository->find($id);
-        if (!$env instanceof Env) {
-            throw $this->createNotFoundException(sprintf('Env "%s" not found.', $id));
-        }
-
-        $form = $this->createForm(EnvType::class, $env, [
-            'is_edit' => true,
-        ]);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_env_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('admin/env/edit.html.twig', [
-            'env' => $env,
-            'form' => $form,
-        ]);
-    }
-
     #[Route('/admin/e/{id}', name: 'app_env_delete', methods: ['POST'])]
     public function delete(Request $request, string $id, EnvRepository $envRepository, EntityManagerInterface $entityManager): Response
     {
@@ -95,35 +70,6 @@ final class EnvController extends AbstractController
         }
 
         return $this->redirectToRoute('app_env_index', [], Response::HTTP_SEE_OTHER);
-    }
-
-    #[Route('/admin/e/parent/{id}/edit', name: 'app_env_parent_edit', methods: ['GET', 'POST'])]
-    public function editParent(
-        Request $request,
-        string $id,
-        EnvParentRepository $envParentRepository,
-        EntityManagerInterface $entityManager,
-    ): Response {
-        $envParent = $envParentRepository->find($id);
-        if (!$envParent instanceof EnvParent) {
-            throw $this->createNotFoundException(sprintf('Env parent "%s" not found.', $id));
-        }
-
-        $form = $this->createForm(EnvParentType::class, $envParent, [
-            'is_edit' => true,
-        ]);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_env_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('admin/env_parent/edit.html.twig', [
-            'env_parent' => $envParent,
-            'form' => $form,
-        ]);
     }
 
     #[Route('/admin/e/parent/{id}', name: 'app_env_parent_delete', methods: ['POST'])]
